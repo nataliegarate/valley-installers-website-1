5832dc6a8e9738ad0e109352bad4682b50d10d15
nataliegarate <natalieegarate@hotmail.com>
2020-03-31 21:08:33 -0700

    added home view from old project

--- components/Footer.js
+++ components/Footer.js
@@ -10,53 +10,53 @@ export default function footer() {
         <div id="space"></div>
         <div id="text">Website Built by Natalie Garate and Lillian Gales</div>
       </div>
-
       <style>
-        {`.contact-footer {
-  padding: 25px 0px;
-  background-color: #fafafa;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  width: 100%;
-}
+        {`
+        .contact-footer {
+            padding: 25px 0px;
+            background-color: #fafafa;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-wrap: wrap;
+            width: 100%;
+        }
 
-.contact-footer h2 {
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 300;
-  letter-spacing: 2px;
-  text-shadow: 1px 1px 0px rgba(255, 255, 255, 0.5);
-  text-align: center;
-  margin: 0px;
-  overflow: hidden;
-}
+        .contact-footer h2 {
+            font-size: 20px;
+            font-style: normal;
+            font-weight: 300;
+            letter-spacing: 2px;
+            text-shadow: 1px 1px 0px rgba(255, 255, 255, 0.5);
+            text-align: center;
+            margin: 0px;
+            overflow: hidden;
+        }
 
-.author-footer {
-  background-color: #282828;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 70px;
-  width: 100%;
-}
+        .author-footer {
+            background-color: #282828;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 70px;
+            width: 100%;
+        }       
 
-.author-footer #text {
-  display: flex;
-  color: #a6a6a6;
-  font-size: 14px;
-  text-shadow: none;
-  font-weight: 300;
-  flex-basis: 95%;
-  align-items: center;
-  justify-content: flex-start;
-}
+        .author-footer #text {
+            display: flex;
+            color: #a6a6a6;
+            font-size: 14px;
+            text-shadow: none;
+            font-weight: 300;
+            flex-basis: 95%;
+            align-items: center;
+            justify-content: flex-start;
+        }
 
-.author-footer #space {
-  flex-basis: 5%;
-}
-`}
+        .author-footer #space {
+            flex-basis: 5%;
+        }
+    `}
       </style>
     </div>
   );
---
+++ pages/home-view.js
@@ -0,0 +1,98 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
+
+function HomeView(props) {
+  return (
+    <>
+      <Header />
+      <div className="content-view">
+        <div id="countertop-text" className="col-1">
+          <h2> Laminate Countertops</h2>
+          <p className="countertop-sub-text">
+            We specialize in laminate countertops. We use Wilsonart, Formica or
+            Nevamar laminate giving you a wide range of selection. We also offer
+            you a variety of edging, from a standard square edge to a beveled
+            edge.
+          </p>
+          <p className="countertop-sub-text">
+            With the Wilsonart HD and the Formica FX lines, your laminate
+            countertops can look like traditional hardsurface tops at a fraction
+            of the cost.
+          </p>
+          <p className="countertop-sub-text">
+            There are more benefits to laminate that just the price. They are
+            more bacterial and scratch resistant than hardsurface. And you have
+            a greater variety of colors and patterns to choose from.
+          </p>
+          <p className="countertop-sub-text">
+            We are also proud to introduce you to our process of installing
+            undermount sink in laminate. So yes you can have an undermount sink
+            with a laminate top!
+          </p>
+
+          <p className="countertop-sub-text" id="see-photos">
+            <Link id="see-gallery" to="/gallery">
+              See All Photos →
+            </Link>{" "}
+          </p>
+        </div>
+        <div className="col-2">
+          <img
+            className="countertops-photo"
+            src="https://i.imgur.com/3SG4Moc.jpg?1"
+          />
+        </div>
+      </div>
+      <Footer />
+      <style>{`
+        .col-2 img {
+            width: inherit;
+            }
+
+        .countertop-text {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding: 0px;
+            align-items: flex-end;
+            margin-right: 20px;
+        }
+
+        .countertop-sub-text {
+            text-align: start;
+            margin: 10px 0px;
+        }
+
+        #countertop-text h2 {
+            text-align: start;
+            width: inherit;
+        }
+
+        #see-gallery {
+            text-align: center;
+            width: inherit;
+            font-size: 16px;
+            color: #4285f4;
+            font-weight: bold;
+            letter-spacing: 1px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        @media only screen and (max-width: 870px) {
+        #see-photos {
+            padding: 15px 0px;
+        }
+        #countertop-text h2 {
+            margin-top: 0px;
+        }
+    }
+`}</style>
+    </>
+  );
+}
+
+export default HomeView;
